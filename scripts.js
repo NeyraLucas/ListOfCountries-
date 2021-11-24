6dd2cd0ae02684551807a1db9f78f876fbe2bb19
NeyraLucas <luiscrazypeople@gmail.com>
2021-11-24 15:44:24 -0600

    workshop 2

--- scripts.js
+++ scripts.js
@@ -1,6 +1,4 @@
 const url = `https://restcountries.com/v3.1/all`;
-const div = document.getElementById("response");
-const p = document.createElement("p");
 const table = document.getElementById("td_body");
 const table2 = document.getElementById("table");
 const search = document.getElementById("searchBox");
@@ -46,7 +44,7 @@ function starData(countriesData) {
 
     options += `
         <tr id="${countries[i].name.common}">
-        <td> ${countries[i].name.common} </td>
+        <td class="check">${countries[i].name.common}</td>
         <td> ${countries[i].capital} </td>
         <td> ${countries[i].region} </td>
         <td> ${leng} </td>
@@ -58,7 +56,13 @@ function starData(countriesData) {
     table.innerHTML += options;
   }
 
+  //console.log(countries);
+
   filters();
+  const borders = document.querySelectorAll(".check");
+  // console.log(borders);
+  //tableBorder.deleteBorder();
+  tableBorder.showBorders(borders, countries);
 }
 
 //get Data wiki
@@ -83,8 +87,8 @@ function dataWiki(name) {
 //filtro y paginación
 function filters() {
   let options = {
-    numberPerPage: 10,
-    constNumberPerPage: 10,
+    numberPerPage: 5,
+    constNumberPerPage: 5,
     numberOfPages: 0,
     goBar: false,
     pageCounter: true,
@@ -99,10 +103,67 @@ function filters() {
 }
 
 //evento onclick para la table
-$("#table")
-  .find("tbody tr")
-  .on("click", () => {});
+
 $(document).on("click", "tr", function (e) {
   let pais = $(this).find("td:eq(0)").text();
   dataWiki(pais);
 });
+
+//get table
+const tableBorder = {
+  showBorders: (arr, countries) => {
+    const tableB = document.getElementById("td_borders");
+    const bord = [];
+    let otp = "";
+    //let filt = {border: ''};
+    let filt = [];
+    arr.forEach((a) => {
+      bord.push(a.innerHTML);
+    });
+
+    //filtrar
+    for (let i = 0; i < bord.length; i++) {
+      for (let j = 0; j < countries.length; j++) {
+        if (bord[i] == countries[j].name.common) {
+          filt.push(countries[j].borders);
+          //filt.border=countries[j].borders;
+          //console.log(filt);
+        }
+      }
+    }
+
+    //code
+    /* for(let i=0; i<filt.length; i++){
+      for(let j=0; j<filt[i].length; j++){
+        searchCode.code(filt[i][j]);
+      }
+    } */
+
+    //console.log('soy: filt ' + filt.length +filt[0].length);
+
+    //printBorders.print(bord);
+    //console.log(bord);
+    for (let i = 0; i < bord.length; i++) {
+      otp = `
+        <tr>
+        <td> ${bord[i]} </td>
+        <td> ${filt[i]} </td>
+        </tr>
+        `;
+      tableB.innerHTML += otp;
+    }
+    //console.log(filt);
+  },
+  deleteBorder:() =>{
+    const tableB = document.getElementById("td_borders");
+    tableB.innerHTML = '';
+  }
+};
+
+//search code
+const searchCode = {
+  code: (code) => {
+    console.log("Es el codigo: " + code);
+  },
+};
+
