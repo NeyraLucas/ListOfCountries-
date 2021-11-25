ef3df3c62af201a2325b9068e8f3ad7a1ad8029d
NeyraLucas <luiscrazypeople@gmail.com>
2021-11-25 00:58:13 -0600

    workshop 2 se agrego la traduccion a la tabla

--- scripts.js
+++ scripts.js
@@ -2,8 +2,13 @@ const url = `https://restcountries.com/v3.1/all`;
 const table = document.getElementById("td_body");
 const table2 = document.getElementById("table");
 const search = document.getElementById("searchBox");
+const btnBorder = document.getElementById("btnBorders");
 let countries;
 let nombre;
+const paises=[];
+const borderTable = document.getElementById("td_borders");//borders
+
+btnBorder.addEventListener("click", eventBorders);
 
 fetch(url)
   .then((res) => res.json())
@@ -28,6 +33,8 @@ function starData(countriesData) {
 
     return 0;
   });
+
+  //console.log(countries);
 
   //iterar datos
   for (let i = 0; i < countries.length; i++) {
@@ -59,10 +66,11 @@ function starData(countriesData) {
   //console.log(countries);
 
   filters();
-  const borders = document.querySelectorAll(".check");
+  //const borders = document.querySelectorAll(".check");
   // console.log(borders);
   //tableBorder.deleteBorder();
-  tableBorder.showBorders(borders, countries);
+  //tableBorder.showBorders(borders, countries);
+
 }
 
 //get Data wiki
@@ -84,6 +92,151 @@ function dataWiki(name) {
     .catch((err) => console.log("Error:", err));
 }
 
+//event btn
+function eventBorders() {
+  const borders = document.querySelectorAll(".check");
+  borderTable.innerHTML ='';
+  for (let i = 0; i < borders.length; i++) {
+    fetch(`https://restcountries.com/v3.1/name/${borders[i].outerText}`)
+      .then((response) => response.json())
+      .then((data) => viewBorder(data))
+      .catch((err) => console.log("Error:", err));
+  }
+  //console.log(borders[0]);
+}
+
+function viewBorder(data) {
+  const bor = data;
+  
+  let option = "";
+  //console.log(data);
+  const testPais = data.map(function (count) {
+    return count.borders;
+  });
+  console.log('borders: ' + testPais);
+  for (let i = 0; i < bor.length; i++) {
+    
+    if (bor.length === undefined) {
+      i++;
+    } else {
+      //console.log(bor[i].name.common);
+      for (let j = 0; j < data[i].borders.length; j++) {
+          //lenguajes(data[i].borders[j]);
+          //let lenguages = lenguajes(data[i].borders[j]);
+          //console.log(lenguages);
+          //data[i].translations.spa.official
+          //paises.push(`https://restcountries.com/v3.1/alpha/${data[i].borders[j]}`);
+          
+         //paises = data[i].borders[j]; 
+          //paises.push(p);
+         // console.log(paises);
+        //////////////////
+        
+        fetch(`https://restcountries.com/v3.1/alpha/${testPais[i][j]}`)
+        .then(resp => resp.json())
+        .then(datos =>{
+         
+          option = `
+          <tr>
+          <th>${bor[i].name.common}</th>
+          <th>${data[i].borders[j]}</th>
+          <th>${datos[0].translations.spa.official}</th>
+          </tr>
+          
+          `;
+          borderTable.innerHTML += option;
+        });
+         /////////////
+         /*  option = `
+          <tr>
+          <th>${bor[i].name.common}</th>
+          <th>${data[i].borders[j]}</th>
+          
+          </tr>
+          
+          `; */
+          //<th>${data[i].translations.spa.official}</th>
+          
+      } //end for
+    }
+  }
+
+  /* const testMap = companies.map(function(company){
+    return `${company.name} [${company.start} - ${company.end}]`;
+}) */
+///////////////////////////
+/*   const testPais = data.map(function (count) {
+    return count.borders;
+  });
+  //let account = '';
+  console.log(testPais);
+  for(let i=0; i<testPais.length; i++){
+    console.log('pais: ' + testPais[i]);
+    for(let j=0; j<testPais[i].length; j++){
+      console.log('pais2: ' + testPais[i][j]);
+      let account = document.createElement('th');
+      //account.push(`https://restcountries.com/v3.1/alpha/${testPais[i][j]}`);
+      fetch(`https://restcountries.com/v3.1/alpha/${testPais[i][j]}`)
+      .then(resp => resp.json())
+      .then(data =>{
+        console.log(data[0].translations.spa.official);
+        account.innerHTML =`
+
+        <th>${data[0].translations.spa.official}</th>
+
+        `
+        borderTable.appendChild(account);
+      });
+
+    }
+  } */
+  ///////////////////////////
+  //console.log(account);
+  
+
+  //fetching(testPais);
+  //const paisesCopy = paises;
+  //console.log(paises[16]);
+
+}
+
+//leng
+//https://restcountries.com/v3.1/alpha/{code}
+/* function lenguajes(len) {
+  fetch(`https://restcountries.com/v3.1/alpha/${len}`)
+    .then((res) => res.json())
+    .then((data) => () => {
+      console.log(data);
+    })
+    .catch((err) => console.log("Error:", err));
+} */
+const getDiv = document.getElementById("prueba");
+// función que implementa un wait con promesas
+const wait = (ms) => new Promise((r, j) => setTimeout(r, ms));
+
+// función que emula un proceso asíncrono
+const fetchUrl = async (url) => {
+  await wait(0);
+  //let options= "";
+  return fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+        console.log(data[0].translations.spa.official);
+       // options =`<p>${data[0].translations.spa.official}</p>`
+        //getDiv.innerHTML += options;
+    });
+    
+    
+};
+
+const fetching = async (arr) => {
+  for (const url of arr) {
+    const data = await fetchUrl(url);
+    //console.log(data);
+  }
+};
+
+
 //filtro y paginación
 function filters() {
   let options = {
@@ -99,7 +252,7 @@ function filters() {
     el: "#searchBox",
   };
 
-  paginate.init(".table", options, filterOptions);
+  paginate.init(".table_countries", options, filterOptions);
 }
 
 //evento onclick para la table
@@ -136,6 +289,7 @@ const tableBorder = {
     /* for(let i=0; i<filt.length; i++){
       for(let j=0; j<filt[i].length; j++){
         searchCode.code(filt[i][j]);
+        console.log()
       }
     } */
 
@@ -152,12 +306,13 @@ const tableBorder = {
         `;
       tableB.innerHTML += otp;
     }
+
     //console.log(filt);
   },
-  deleteBorder:() =>{
+  deleteBorder: () => {
     const tableB = document.getElementById("td_borders");
-    tableB.innerHTML = '';
-  }
+    tableB.innerHTML = "";
+  },
 };
 
 //search code
